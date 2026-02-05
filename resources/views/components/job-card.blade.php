0474a79e1a9027263ae2cfd46dd46ff0b246e6bc
Truuuuuuuu <jethruel@gmail.com>
2026-02-06 01:03:16 +0800

    added components for tags, cards

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>tag1</x-tag>
+            <x-tag>tag2</x-tag>
+            <x-tag>tag3</x-tag>
+        </div>
+        <img src="https://dummyimage.com/42x42/000/fff" alt="" class="rounded-xl">
+    </div>
+</div>
