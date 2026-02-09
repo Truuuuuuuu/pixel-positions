a36ab9f228d50c644325652ccc4eb1f57116ec35
Truuuuuuuu <jethruel@gmail.com>
2026-02-09 17:57:58 +0800

    added Tags, used sequence, separated card tru featured'

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,19 +1,20 @@
+@props(['job'])
 <x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo/>
     </div>
 
     <div class="flex-1 flex flex-col">
-        <a href="" class="self-start text-sm text-gray-500 ">Laracasts</a>
-        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full time - From $60,000</p>
+        <a href="" class="self-start text-sm text-gray-500 ">{{$job->employer->name}}</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-800 transition-colors duration-300">{{$job->title}}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{$job->salary}}</p>
     </div>
 
     <div>
         <div>
-            <x-tag>tag</x-tag>
-            <x-tag>tag</x-tag>
-            <x-tag>tag</x-tag>
+            @foreach($job->tags as $tag)
+                <x-tag :$tag />
+            @endforeach
         </div>
     </div>
 </x-panel>
---
+++ tests/Unit/JobTest.php
@@ -0,0 +1,25 @@
+<?php
+///it: keyword is important
+use App\Models\Employer;
+use App\Models\Job;
+
+
+test('it belongs to an employer', function ()  {
+    //Arrange
+    $employer = Employer::factory()->create();
+    $job = Job::factory()->create([
+        'employer_id' => $employer->id,
+    ]);
+
+    //Act and Assert
+    expect($job->employer->is($employer))->toBeTrue();
+
+});
+
+it('can have a tags', function (){
+    $job = Job::factory()->create();
+
+    $job->tag('Frontend');
+
+    expect($job->tags)->toHaveCount(1);
+});
